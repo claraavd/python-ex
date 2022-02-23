0f23efe6679555ba1e397b54ee08c9133b473cf8
claraavd <claravieiradiniz@gmail.com>
2022-02-23 20:14:46 -0300

    ex021 - MP3 Player (Curso em Vídeo)

--- exercicios/ex021.py
+++ exercicios/ex021.py
@@ -1,3 +1,24 @@
+# importing vlc module
 import vlc
 
-media = vlc.MediaPlayer()
+# importing time module
+import time
+
+
+# creating vlc media player object
+media_player = vlc.MediaPlayer("file:///home/fabriciofr/Music/claraavd/python-ex/exercicios/ex021.mp3")
+
+
+# start playing video
+media_player.play()
+
+# wait so the video can be played for 5 seconds
+# irrespective for length of video
+time.sleep(10)
+
+# getting media
+value = media_player.get_media()
+
+# printing media
+print("Media : ")
+print(value)
